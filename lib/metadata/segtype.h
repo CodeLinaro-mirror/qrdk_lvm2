sailfishos-mirror/lvm2

vdo: add support for VDO kernel direct formatting
VDO has added a feature (dm-vdo 9.2.0+) to format devices directly
in its kernel code vs using the vdoformat userspace tool. Add support
for this via a new config parameter vdo_use_kernel_format.

When vdo_use_kernel_format=1, LVM zeroes the first 4KiB of the pool
data LV and passes slabSize, indexMemory, and indexSparse parameters
on the device-mapper table line so the kernel formats on activation.

The use_kernel_format flag is persisted in VDO pool metadata so it
survives reactivation. Old metadata without the flag defaults to 0
(userspace vdoformat), ensuring backward compatibility.

Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>
Made-with: Cursor

lib/metadata/segtype.h
@@ -355,6 +355,7 @@ int init_vdo_segtypes(struct cmd_context *cmd, struct segtype_library *seglib);
 
 #define VDO_FEATURE_ONLINE_RENAME		(1U << 0) /* version 6.2.3 */
 #define VDO_FEATURE_VERSION4			(1U << 1) /* version 8.2.0 */
+#define VDO_FEATURE_DIRECT_FORMAT		(1U << 2) /* version 9.2.0 */
 
 int init_writecache_segtypes(struct cmd_context *cmd, struct segtype_library *seglib);
 

CodeLinaro-mirror/qrdk_lvm2

lib/metadata/segtype.h
@@ -356,6 +356,7 @@ int init_vdo_segtypes(struct cmd_context *cmd, struct segtype_library *seglib);
 
 #define VDO_FEATURE_ONLINE_RENAME		(1U << 0) /* version 6.2.3 */
 #define VDO_FEATURE_VERSION4			(1U << 1) /* version 8.2.0 */
+#define VDO_FEATURE_KERNEL_FORMAT		(1U << 2) /* version 9.2.0 */
 
 int init_writecache_segtypes(struct cmd_context *cmd, struct segtype_library *seglib);
 